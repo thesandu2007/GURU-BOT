e6ecaa143e63aab003ea908695ba8155e4c0bfd6
thesandu2007 <148632690+thesandu2007@users.noreply.github.com>
2024-04-21 14:03:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94725864680', 'ඩෝ̶ᴾ̶ᴇ̶:͢͠තේ͜ᴱ̶̸ꜱ͞ᴜ͞', true],
+  ['94729646849', 'THESSA FF', true], 
   [''] 
 ] //Number of owners
 
